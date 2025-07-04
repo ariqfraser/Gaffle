263236c0f320f098c130fa2ed42b06d866185b90
ariq <ariqfraser@gmail.com>
2025-07-04 09:21:52 +0100

    project setup

---
+++ gaffle-backend/src/main/java/dev/ariqfraser/gaffle/GaffleApplication.java
@@ -0,0 +1,13 @@
+package dev.ariqfraser.gaffle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaffleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaffleApplication.class, args);
+	}
+
+}
---
+++ gaffle-backend/src/test/java/dev/ariqfraser/gaffle/GaffleApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ariqfraser.gaffle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaffleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
